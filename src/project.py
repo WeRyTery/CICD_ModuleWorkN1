794cd7bc5d73be6f0a7ea07340453168fdc08c5d
Ghost <133059737+WeRyTery@users.noreply.github.com>
2026-04-08 14:41:32 +0300

    Workflows: added CI wordflow
    - Added CI workflow configured file
    - Added .flake8 configuration file
    - Fixed .flake8 style problems

--- src/project.py
+++ src/project.py
@@ -1,6 +1,7 @@
 import re
 
-def analyze_text(file_path : str):
+
+def analyze_text(file_path: str):
     with open(file_path, encoding='utf-8') as file:
         text = file.read()
 
@@ -10,10 +11,11 @@ def analyze_text(file_path : str):
 
     # count amount of words in text
     words = re.split(r'[ \t\n,:;]+', text)
-    words = [w for w in words if w.strip() != ''] # if no words given
+    words = [w for w in words if w.strip() != '']  # if no words given
     word_count = len(words)
 
     return word_count, sentence_count
+
 
 def main():
     file_path = "C:\\Users\\Ghost\\Desktop\\Univ\\Document_data.txt"
@@ -22,5 +24,6 @@ def main():
 
     print(f"Words count: {words}")
     print(f"Sentence count: {sentences}")
+
 
 main()
