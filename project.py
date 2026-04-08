199a715be8165f08c4c0489c26e60c4fd0ccd336
Ghost <133059737+WeRyTery@users.noreply.github.com>
2026-04-08 13:56:44 +0300

    feature: added method for text analisis
    Method accepts file path and returns 2 strings with words count and sentence count

---
+++ project.py
@@ -0,0 +1,16 @@
+import re
+
+def analyze_text(file_path : str):
+    with open(file_path, encoding='utf-8') as file:
+        text = file.read()
+
+    # count amount of sentence enders
+    sentences = re.findall(r'\.\.\.|[.!?]', text)
+    sentence_count = len(sentences)
+
+    # count amount of words in text
+    words = re.split(r'[ ,:;]+', text)
+    words = [w for w in words if w.strip() != ''] # if no words given
+    word_count = len(words)
+
+    return word_count, sentence_count
